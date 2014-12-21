c46f4d8fd0d6250b148c508f1ec666e213085eba
David <eodos@eodos.net>
2014-12-21 19:38:08 +0100

    Backup old dotfiles

--- install.sh
+++ install.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 ############################
 # install.sh
-# # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# ############################
+# # This script creates replaces the downloaded dotfiles in your home directory
+############################
 
-# Create bashrc symlink
-echo 'Creating bashrc symlink...'
-ln -s bashrc ~/.bashrc
-echo '...done'
+# Get script directory
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Create vim folder symlink
-echo 'Creating vim folder symlink ...'
-ln -s vim ~/.vim
-echo '...done'
+# Backup present files
+mkdir -p ~/backup-files/
 
-# Create vimrc symlink
-echo 'Creating vimrc symlink ...'
-ln -s vim/vimrc ~/.vimrc
-echo '...done'
+if [ -f ~/.bashrc ]
+then
+  mv ~/.bashrc ~/backup-files/.bashrc
+  echo "Current .bashrc file saved in ~/backup-files/"
+fi
+
+if [ -d ~/.vim/ ]
+then
+  mv ~/.vim ~/backup-files/.vim
+  echo "Current .vim folder saved in ~/backup-files/"
+fi
+
+if [ -f ~/.vimrc ]
+then
+  mv ~/.vimrc ~/backup-files/.vimrc
+  echo "Current .vimrc folder saves in ~/backup-files/"
+fi
+
+# Replace bashrc file
+echo "Replacing bashrc file..."
+cp $DIR/bashrc ~/.bashrc
+
+# Replace vim folder
+echo "Replacing vim folder..."
+cp -R $DIR/vim ~/.vim
+
+# Replace vimrc folder
+echo "Replacing vimrc file..."
+cp $DIR/vim/vimrc ~/.vimrc
 
